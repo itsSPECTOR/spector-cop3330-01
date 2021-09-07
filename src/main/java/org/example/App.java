dcf2683803da129af11fea292624aa5453e925ff
Reese Spector <89818858+itsSPECTOR@users.noreply.github.com>
2021-09-07 10:07:24 -0400

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,12 @@
 package org.example;
 import java.util.Scanner;  // Import the Scanner class
 
-/**
- * Hello world!
- *
- */
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+*/
+
 public class App 
 {
     public static void main( String[] args )
